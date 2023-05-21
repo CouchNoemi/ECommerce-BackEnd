f0f6d372f4e5f40a4d520f094e352ee2779ff35c
NoemiCouch <Noemicouch@gmail.com>
2023-05-20 20:05:52 -0500

    added all the routs, for product,category,and tag. Including the get post put and delete functionality

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -1,28 +1,45 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get("/", (req, res) => {
+  Category.findAll({ include: Product })
+    .then((categories) => {
+      res.json(categories);
+    })
+    .catch((err) => res.json(err));
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  Category.findOne({ include: Product, where: { id } })
+    .then((data) => res.json(data))
+    .catch((err) => res.json(err));
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post("/", (req, res) => {
+  const { category_name } = req.body;
+  Category.create({ category_name })
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => res.json(err));
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.put("/:id", (req, res) => {
+  const { category_name } = req.body;
+  const id = req.params.id;
+  Category.update({ category_name }, { where: { id } })
+    .then((data) => res.json(data))
+    .catch((err) => res.json(err));
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+router.delete("/:id", (req, res) => {
+  const id= req.params.id;
+  Category.destroy({where:{id}})
+  .then(data => res.json(data))
+  .catch(err => res.json(err))
 });
 
 module.exports = router;
--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -1,28 +1,45 @@
-const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const router = require("express").Router();
+const { Tag, Product, ProductTag } = require("../../models");
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+router.get("/", (req, res) => {
+  Tag.findAll({ include: Product })
+    .then((tags) => {
+      res.json(tags);
+    })
+    .catch((err) => res.json(err));
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  Tag.findOne({ include: Product, where: { id } })
+    .then((data) => res.json(data))
+    .catch((err) => res.json(err));
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
+router.post("/", (req, res) => {
+  const { tag_name } = req.body;
+  Tag.create({ tag_name })
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => res.json(err));
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+router.put("/:id", (req, res) => {
+  const { tag_name } = req.body;
+  const id = req.params.id;
+  Tag.update({ tag_name }, { where: { id } })
+    .then((data) => res.json(data))
+    .catch((err) => res.json(err));
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
+  Tag.destroy({ where: { id } })
+    .then((data) => res.json(data))
+    .catch((err) => res.json(err));
 });
 
 module.exports = router;
